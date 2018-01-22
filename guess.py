cafc02782a559db838508df3f1f84f0d4b52877a
yddhappiness <yddhappiness@163.com>
2018-01-22 20:45:36 +0800

    the first edition

---
+++ guess.py
@@ -0,0 +1,10 @@
+from random import randint
+i = randint(1,10)
+print(i)
+while True:
+    guess = input("What is your guess number between 1 and 10?")
+    if int(guess) == i:
+        print("goodjob")
+        break
+    else:
+        print("go on")
